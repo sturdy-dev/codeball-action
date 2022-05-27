34978b8faa46662fd8ecdbeb841820d8de3d8cb3
Nikita Galaiko <nikita@galaiko.rocks>
2022-05-27 16:20:07 +0200

    approved: allow dry run

--- src/approver/main.ts
+++ src/approver/main.ts
@@ -1,6 +1,9 @@
 import fetch from 'node-fetch'
 import {Job} from './types'
 import {isContributionJob, isFinalStatus} from './utils'
+import core from '@actions/core'
+import github from '@actions/github'
+import {Octokit} from '@octokit/action'
 
 async function getJob(id: string): Promise<Job> {
   const res = await fetch(`https://api.codeball.ai/jobs/${id}`)
@@ -9,21 +12,30 @@ async function getJob(id: string): Promise<Job> {
 }
 
 async function run(): Promise<void> {
-  const core = require('@actions/core')
-
   try {
-    const github = require('@actions/github')
-    const {Octokit} = require('@octokit/action')
-
     const pullRequestURL = github.context.payload?.pull_request?.html_url
-
     if (!pullRequestURL) {
-      core.setFailed('No pull request URL found')
-      return
+      throw new Error('No pull request URL found')
+    }
+
+    const pullRequestNumber = github.context.payload?.pull_request?.number
+    if (!pullRequestNumber) {
+      throw new Error('No pull request number found')
+    }
+
+    const commitId = github.context.payload.pull_request?.head.sha
+    if (!commitId) {
+      throw new Error('No commit ID found')
     }
 
     const jobID = core.getInput('codeball-job-id')
+    const doApprove = core.getInput('do-approve') === 'true'
+    const doLabel = core.getInput('do-label') === 'true'
+    const labelValue = core.getInput('label')
+
     core.info(`Job ID: ${jobID}`)
+    core.info(`Do approve: ${doApprove}`)
+    core.info(`Do label: ${doLabel}`)
 
     let job = await getJob(jobID)
     let attempts = 0
@@ -38,33 +50,40 @@ async function run(): Promise<void> {
     }
 
     if (!isFinalStatus(job.status)) {
-      core.setFailed(`Job ${jobID} is not finished`)
-      return
+      throw new Error(`Job ${jobID} is not finished`)
     }
 
     if (!isContributionJob(job)) {
-      core.setFailed(`Job ${jobID} is not a contribution job`)
-      return
+      throw new Error(`Job ${jobID} is not a contribution job`)
     }
 
     const approved = job.contribution?.result === 'approved'
 
+    const octokit = new Octokit()
     if (approved) {
       core.info(`Job ${jobID} is approved, approving the PR now!`)
 
-      const octokit = new Octokit()
+      if (doLabel) {
+        core.debug(`Adding label to PR ${pullRequestURL}`)
+        await octokit.issues.addLabels({
+          owner: github.context.repo.owner,
+          repo: github.context.repo.repo,
+          issue_number: pullRequestNumber,
+          labels: [labelValue]
+        })
+      }
 
-      await octokit.request(
-        'POST /repos/{owner}/{repo}/pulls/{pull_number}/reviews',
-        {
-          owner: github.context.payload.organization.login,
-          repo: github.context.payload.repository.name,
-          pull_number: github.context.payload.pull_request.number,
-          commit_id: github.context.payload.pull_request.head.sha,
+      if (doApprove) {
+        core.debug(`Approving PR ${pullRequestURL}`)
+        await octokit.pulls.createReview({
+          owner: github.context.repo.owner,
+          repo: github.context.repo.repo,
+          pull_number: pullRequestNumber,
+          commit_id: commitId,
           body: 'Codeball: LGTM! :+1:',
           event: 'APPROVE'
-        }
-      )
+        })
+      }
     } else {
       core.info(`Job ${jobID} is not approved, will not approve the PR`)
     }
@@ -76,15 +95,9 @@ async function run(): Promise<void> {
           {data: 'Pull Request', header: true},
           {data: 'Result', header: true}
         ],
-        [
-          `#${github.context.payload.pull_request.number}`,
-          approved ? 'Approved ✅' : 'Not approved'
-        ]
+        [`#${pullRequestNumber}`, approved ? 'Approved ✅' : 'Not approved']
       ])
-      .addLink(
-        'View on web',
-        `https://codeball.ai/prediction/${jobID}`
-      )
+      .addLink('View on web', `https://codeball.ai/prediction/${jobID}`)
       .write()
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
