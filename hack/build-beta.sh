eabb178caa0b9d6d059f9277d357b96238b016d5
Gustav Westling <gustav@getsturdy.com>
2022-05-30 16:44:38 +0200

    Fixes for building beta releases of the action

--- hack/build-beta.sh
+++ hack/build-beta.sh
@@ -15,9 +15,9 @@ git add lib dist || true
 git commit -m "Build action" --allow-empty
 
 # Use @beta version of the action
-ls **/action.yml | xargs -n1 gsed -i 's/@v1/@beta/'
-git add **/action.yml || true
+find . -type f -name "action.yml" -exec gsed -i 's/@v1/@beta/' {} \;
+find . -type f -name "action.yml" -exec git add {} \;
 git commit -m "Use beta version" --allow-empty
 
 # Push
-git push -u origin beta:beta
+git push -u origin beta:beta --force
