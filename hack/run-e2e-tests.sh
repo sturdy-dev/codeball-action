7837bf3e4d1aa5c3625e6a12a4becbabbeaef703
Nikita Galaiko <nikita@galaiko.rocks>
2022-06-01 11:19:27 +0200

    hack/e2e: sanity checks

--- hack/run-e2e-tests.sh
+++ hack/run-e2e-tests.sh
@@ -2,9 +2,12 @@
 
 set -euo pipefail
 
+[[ -z $(command -v act) ]] && echo "act not found, install https://github.com/nektos/act" && exit 1
+[[ -z ${GITHUB_TOKEN} ]] && echo "GITHUB_TOKEN env variable is required. obtail one from https://github.com/settings/tokens/new" && exit 1
+
 yarn build
 yarn package
 
 act pull_request -e tests/act-pull-request.json \
-  -s GITHUB_TOKEN=${GITHUB_TOKEN} \
-  --env CODEBALL_API_HOST=http://host.docker.internal:8080
+	-s GITHUB_TOKEN=${GITHUB_TOKEN} \
+	--env CODEBALL_API_HOST=http://host.docker.internal:8080
