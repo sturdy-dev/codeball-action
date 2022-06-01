2dc1433af12ba143e03461789b9603a311612f9a
Nikita Galaiko <nikita@galaiko.rocks>
2022-06-01 12:54:15 +0200

    refactor: extract api calls to a shared lib

--- src/baller/main.ts
+++ src/baller/main.ts
@@ -1,16 +1,9 @@
-import fetch from 'node-fetch'
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-
-type JobResponse = {
-  id: string
-  status: string
-}
+import {create} from '../lib'
 
 async function run(): Promise<void> {
   try {
-    const hostName = process.env.CODEBALL_API_HOST || 'https://api.codeball.ai'
-
     const pullRequestURL = github.context.payload?.pull_request?.html_url
     if (!pullRequestURL) {
       core.setFailed('No pull request URL found')
@@ -25,20 +18,13 @@ async function run(): Promise<void> {
 
     core.info(`Found contribution: ${pullRequestURL}`)
 
-    const data = {
+    const job = await create({
       url: pullRequestURL,
       access_token: githubToken
-    }
-
-    const response = await fetch(`${hostName}/jobs`, {
-      method: 'POST',
-      body: JSON.stringify(data)
     })
 
-    const resData = (await response.json()) as JobResponse
-
-    core.info(`Job created: ${resData.id}`)
-    core.setOutput('codeball-job-id', resData.id)
+    core.info(`Job created: ${job.id}`)
+    core.setOutput('codeball-job-id', job.id)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
