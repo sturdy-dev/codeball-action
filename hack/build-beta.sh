44f2f318a486ee5e11a931342ff26f4ef3a106e6
Gustav Westling <gustav@getsturdy.com>
2022-06-09 14:12:07 +0200

    Fix imports of github libraries

--- hack/build-beta.sh
+++ hack/build-beta.sh
@@ -8,14 +8,15 @@ git branch -D beta || true
 
 git checkout -b beta
 
+rm -rf lib dist
+
 # Build
 yarn build
-yarn package
-git add lib dist || true
+git add dist || true
 git commit -m "Build action" --allow-empty
 
 # Use @beta version of the action
-find . -type f -name "action.yml" -exec gsed -i 's/@v1/@beta/' {} \;
+find . -type f -name "action.yml" -exec gsed -i 's/@v2/@beta/' {} \;
 find . -type f -name "action.yml" -exec git add {} \;
 git commit -m "Use beta version" --allow-empty
 
