ced1046934d51513f8a6c2379d9bd5be16403b7b
Gustav Westling <gustav@getsturdy.com>
2022-09-08 15:22:16 +0200

    labeler: track set labelName

--- src/lib/track/index.ts
+++ src/lib/track/index.ts
@@ -3,14 +3,17 @@ import {post} from '../api'
 export const track = async ({
   jobID,
   actionName,
-  error
+  error,
+  data
 }: {
   jobID?: string
   actionName: string
   error?: string
+  data?: any
 }) =>
   post('/track', {
     job_id: jobID ?? null,
     name: actionName,
-    error: error ?? null
+    error: error ?? null,
+    data: data ?? null
   }).catch(error => console.warn(error))
